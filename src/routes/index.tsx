fbd1955e294ca2cea7be8966972eb3eaee6a48a5
sir-msimango <mr.msimango.s@outlook.com>
2026-09-12 13:59:12 +0200

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: Home })
+
+function Home() {
+  return (
+    <main>
+      <h1>Hello, TanStack Start!</h1>
+    </main>
+  )
+}
